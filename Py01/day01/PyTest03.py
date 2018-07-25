57ab46e647c828d743d14e079ea6d41ce80ddeb4
lingxiaodshz <lingxiaodshz@163.com>
2018-07-25 20:56:25 +0800

    Python set

--- Py01/day01/PyTest03.py
+++ Py01/day01/PyTest03.py
@@ -26,3 +26,39 @@ d = {
 }
 for k in d:
     print(k, ':', d[k])
+
+# set的内部结构和dict很像，唯一区别是不存储value，因此，判断一个元素是否在set中速度很快。
+# set存储的元素和dict的key类似，必须是不变对象，因此，任何可变对象是不能放入set中的。
+# set存储的元素也是没有顺序的。
+
+months = set(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+x1 = 'Feb'
+x2 = 'Sun'
+
+if x1 in months:
+    print('x1: ok')
+else:
+    print('x1: error')
+
+if x2 in months:
+    print('x2: ok')
+else:
+    print('x2: error')
+
+# 请用 for 循环遍历如下的set，打印出 name: score 来。
+s = set([('Adam', 95), ('Lisa', 85), ('Bart', 59)])
+for x in s:
+    print(x[0], ':', x[1])
+
+# 针对下面的set，给定一个list，对list中的每一个元素，
+# 如果在set中，就将其删除，如果不在set中，就添加进去。
+# s = set(['Adam', 'Lisa', 'Paul'])
+# L = ['Adam', 'Lisa', 'Bart', 'Paul']
+s = set(['Adam', 'Lisa', 'Paul'])
+L = ['Adam', 'Lisa', 'Bart', 'Paul']
+for x in L:
+    l = len(s)
+    s.add(x)
+    if l == len(s):
+        s.remove(x)
+print(s)
