a010c39b112361142192604c7bd4b89f08ccf23c
lingxiaodshz <lingxiaodshz@163.com>
2018-08-20 21:11:22 +0800

    Python字符串的常用方法

--- Py01/day02/Day02Test.py
+++ Py01/day02/Day02Test.py
@@ -12,7 +12,7 @@ print(str[-1:0:-1])  # 无法取到第一位
 print(str[-1::-1])  # 这样是完整的逆序
 
 # 字符串的方法
-str2 = "I am a student.I am 15 years old."
+str2 = "I am a student. I am 15 years old."
 # find类似于Java中的indexOf
 print(str2.find("am"))
 # rfind从右往左查找
@@ -26,3 +26,40 @@ print(str2.count("am1"))
 # replace替换后原来的值不会变
 print(str2.replace("am", "is"))
 print(str2)
+# split,startswith,endswith
+print(str2.split(" "))
+print(str2.startswith("I"))
+print(str2.endswith("old"))
+# lower,upper
+print(str2.lower())
+print(str2.upper())
+# center(Int),ljust(Int),rjust(Int) 在某个宽度居中，靠左，靠右
+print(str2.center(50))
+print(str2.ljust(50))
+print(str2.rjust(50))
+# strip,lstrip,rstrip 去掉两侧，左侧，右侧的空格
+str3 = "     hello world      "
+print(str3.strip())
+print(str3.lstrip())
+print(str3.rstrip())
+# partition,rpartition 以传入的内容分三段
+print(str2.partition("am"))
+print(str2.rpartition("am"))
+
+# isslpha,isdigit，isalnum 是字母，是数字,既有数字又有字母
+# num = input("请输入一个字符：")
+# if num.isalpha():
+#     print("输入的是字母")
+# elif num.isdigit():
+#     print("输入的是数字")
+# elif num.isalnum():
+#     print("输入的既有数字又有字母")
+
+# a.join(b), 以a为分割，连接b
+a = " "
+b = ["hello","world","hello"]
+print(a.join(b))
+# 如果有空格和换行符同时存在，又需要切割，可直接用split()，它会对空白字符进行切割
+# 这个有些版本可以实现，有些不能
+str4 = "sad hfk lsd/thg ksl /t fhl gks /t hfd/tkgh lsk /tfghlksgl"
+print(str4.split())
