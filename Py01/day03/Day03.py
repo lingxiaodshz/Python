2ec4cb36b21d10729415b2a7590ea7cb594c8d26
lingxiaodshz <lingxiaodshz@163.com>
2018-09-01 21:38:38 +0800

    Python字符串和List的切片

--- Py01/day03/Day03.py
+++ Py01/day03/Day03.py
@@ -21,6 +21,50 @@ def average(*args):
         sum += num
     return sum / len(args)
 
+
 print(average())
 print(average(1, 2))
 print(average(1, 2, 2, 3, 4))
+
+'''
+range()函数可以创建一个数列：
+>>> range(1, 101)
+[1, 2, 3, ..., 100]
+请利用切片，取出：
+1. 前10个数；
+2. 3的倍数；
+3. 不大于50的5的倍数。
+'''
+L = range(1, 101)
+# L[0:len(L):1] 注意：list从下标0开始，len(L)代表list长度，最后的1代表间隔，默认是1
+print(L[:10])
+print(L[2::3])
+print(L[4:50:5])
+
+print("----------------------------")
+'''
+利用倒序切片对 1 - 100 的数列取出：
+* 最后10个数；
+* 最后10个5的倍数。
+'''
+L = range(1, 101)
+print(L[-10:])
+print(L[-46::5])
+
+'''
+字符串有个方法 upper() 可以把字符变成大写字母：
+>>> 'abc'.upper()
+'ABC'
+但它会把所有字母都变成大写。请设计一个函数，它接受一个字符串，
+然后返回一个仅首字母变成大写的字符串。
+提示：利用切片操作简化字符串操作。
+'''
+
+
+def firstCharUpper(s):
+    return s[:1].upper() + s[1:]
+
+
+print(firstCharUpper('hello'))
+print(firstCharUpper('sunday'))
+print(firstCharUpper('september'))
